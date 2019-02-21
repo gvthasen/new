b7812353edf09db3e406fbe6bf52d42a917293e7
gvthasen <47817820+gvthasen@users.noreply.github.com>
2019-02-21 15:04:17 +0530

    Add files via upload

---
+++ division.py
@@ -0,0 +1,8 @@
+def modulo(a,b):
+    z=a%b
+    print("modulus of %d and %d is" %(a,b),z)
+modulo(5,8)
+def division(a,b):
+    z=a/b
+    print("the division of %d and %d is" %(a,b),z)
+division(4,5)
